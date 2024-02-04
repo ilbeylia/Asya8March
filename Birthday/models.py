a18d96c6142a12171d4882e01dcd2931330572cb
Abdurrahman ilbeyli <ilbeyli187@gmail.com>
2024-02-04 17:39:00 +0300

    v2

--- Birthday/models.py
+++ Birthday/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Entry(models.Model):
     name = models.CharField(max_length=100)
     text = models.TextField()
-    selected_gif = models.CharField(max_length=255, blank=True, null=True)
+    selected_gif = models.CharField(max_length=255)
 
     def __str__(self):
         return self.name
